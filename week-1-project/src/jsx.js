c81dda0cfb553bdf74e54a38f278884a28bba008
Dena Cho <denaexplore@gmail.com>
2020-02-03 16:42:08 +0100

    jsx test

--- week-1-project/src/jsx.js
+++ week-1-project/src/jsx.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-    const incorrectTitle = 'Wrong title';
-    const title = 'My App';
+  const incorrectTitle = "Wrong title";
+  const title = "My App";
 
-    return (
-        <section>
-            <h1 className={"title"}>{title}</h1>
-            <p data-testid="injection">{'<script>alert("danger");</script>'}</p>
-            <form>
-                <label data-testid="label" htmlFor="input-demo">Demo:</label>
-                <input data-testid="input" type="text" id="input-demo"/>
-            </form>
-        </section>
-    )
+  return (
+    <section>
+      <h1 className={""}>{title}</h1>
+      <p data-testid="injection">{""}</p>
+      <form>
+        <label data-testid="label" htmlFor="">
+          Demo:
+        </label>
+        <input data-testid="input" type="text" id="input-demo" />
+      </form>
+    </section>
+  );
 }
 
 export { App };
