5a8dae33fd4b7a407d65cbca39be1a30b8bcf2e5
Dena Cho <denaexplore@gmail.com>
2020-02-03 16:39:06 +0100

    component test

--- week-1-project/src/component.js
+++ week-1-project/src/component.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-    const result = 23434 * 0.345;
-    return (
-        <section>
-            <h1>The Example App</h1>
-            <NewsArticle />
-            <p>
-                Renders the value of a variable: <span>{result}</span>
-            </p>
-            <button data-testid="load-more" onClick={() => { console.log('The button was clicked') }}>Load More</button>
-        </section>
-    );
+  const result = 23434 * 0.345;
+  return (
+    <section>
+      <h1>The Example App</h1>
+      <NewsArticle />
+      <p>
+        Renders the value of a variable: <span>{result}</span>
+      </p>
+      <button
+        data-testid="load-more"
+        onClick={() => {
+          console.log("The button was clicked");
+        }}
+      >
+        Load More
+      </button>
+    </section>
+  );
 }
 
 function NewsArticle() {
-    return (
-        <article>
-            <h1>Article</h1>
-        </article>
-    )
+  return (
+    <article>
+      <h1>Article</h1>
+    </article>
+  );
 }
 
-export { App };
+function MyApp() {
+  const articles = [];
+  for (let i = 0; i < 200; i++) {
+    articles.push(<NewsArticle />);
+  }
+  return (
+    <div>
+      <p> My First React App</p>
+      {articles}
+    </div>
+  );
+}
+export { App, MyApp };
