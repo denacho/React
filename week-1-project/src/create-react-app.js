dac0e113d5783bcaabe8035797850c4f57e2a68f
Dena Cho <denaexplore@gmail.com>
2020-02-14 21:49:05 +0100

    exercise

--- week-1-project/src/create-react-app.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const optionalChaining = object => {
-  return object?.bar?.buzz;
-};
-const Import = "";
-
-export { optionalChaining, Import };
--- week-1-project/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './props'
-
-// Check the tests in ../public/index.html to see what to fix.
-
-ReactDOM.render(null, document.getElementById('root'));
